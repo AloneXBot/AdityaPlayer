a75137a9b33b5d0f53c84cce343da027dd035cba
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-12-03 15:45:42 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -84,8 +84,8 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", 0))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
-START_IMAGE_URL = getenv("START_IMAGE_URL", None)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002273542888"))
+START_IMAGE_URL = getenv("START_IMAGE_URL", "https://files.catbox.moe/s7iujk.jpg")
 
 
 # Memory Database
